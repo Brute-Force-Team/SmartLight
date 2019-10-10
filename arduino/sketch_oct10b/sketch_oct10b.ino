a082bc45fcc93641be6eaa4275d1db03e326fa59
EncoXXX <bednarsasha@gmail.com>
2019-10-10 22:57:44 +0300

    Serial команди для включення та виключення

--- arduino/sketch_oct10b/sketch_oct10b.ino
+++ arduino/sketch_oct10b/sketch_oct10b.ino
@@ -4,18 +4,36 @@
 CRGB  leds[NUM_LEDS];
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
-  // int index = 0;
-  // char string[20];
-  // while(Serial.available()>0){
-  //   char input_char = Serial.read();
-  //   string[0] = input_char;
-  // }
-  test();
+  int index = 0;
+  char string[20];
+  while(Serial.available()>0){
+    char input_char = Serial.read();
+    string[0] = input_char;
+  }
+  if(string[0]=='Y'){
+    turn_on();
+  }else if(string[0] == 'N'){
+    turn_off();
+  }
+}
+
+void turn_on(){
+  for(int i = 0; i < NUM_LEDS; ++i){
+    leds[i] = CRGB::White;
+  }
+  FastLED.show();
+}
+
+void turn_off(){
+  for(int i = 0; i < NUM_LEDS; ++i){
+    leds[i] = CRGB::Black;
+  }
+  FastLED.show();
 }
 
 void test(){
@@ -26,9 +44,9 @@ void test(){
   // Set the first led back to black for 1 second
   //leds[0] = CRGB::Black; FastLED.show();
   for(int i = 0; i<NUM_LEDS; i++){
-    leds[i] = CRGB::Red;
+    leds[i] = CRGB::White;
     FastLED.show();
-    delay(100);
+    delay(10);
     leds[i] = CRGB::Black;
     FastLED.show();
   }
