1c0cf01dca2046ab27cf82b900c0cc47efcf66b3
EncoXXX <bednarsasha@gmail.com>
2019-10-10 16:33:05 +0300

    Макет сайту управління

--- site/index.php
+++ site/index.php
@@ -1,3 +1,30 @@
-<?php
-  phpinfo();
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Управління</title>
+    <link rel = "stylesheet" href = "index.css">
+  </head>
+  <body>
+    <div class = "line ln1">
+      <div class = "D1"></div>
+      <div class = "D2"></div>
+      <div class = "D3"></div>
+      <div class = "D4"></div>
+      <div class = "D5"></div>
+      <div class = "D6"></div>
+      <div class = "D7"></div>
+      <div class = "D8"></div>
+    </div>
+    <div class="line ln2">
+      <div class = "DD1"></div>
+      <div class = "DD2"></div>
+      <div class = "DD3"></div>
+      <div class = "DD4"></div>
+      <div class = "DD5"></div>
+      <div class = "DD6"></div>
+      <div class = "DD7"></div>
+      <div class = "DD8"></div>
+    </div>
+  </body>
+
+</html>
