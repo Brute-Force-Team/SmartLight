37e48238b0950693ce2cfaeba76574b5b6c6cb16
EncoXXX <bednarsasha@gmail.com>
2019-10-10 22:20:04 +0300

    Тестування адресної лєнти

--- arduino/sketch_oct10b/sketch_oct10b.ino
+++ arduino/sketch_oct10b/sketch_oct10b.ino
@@ -1,9 +1,36 @@
+#include <FastLED.h>
+#define NUM_LEDS 14
+#define DATA_PIN 6
+CRGB  leds[NUM_LEDS];
+
 void setup() {
-  int i = 0;
+  Serial.begin(9600);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
-
+  // int index = 0;
+  // char string[20];
+  // while(Serial.available()>0){
+  //   char input_char = Serial.read();
+  //   string[0] = input_char;
+  // }
+  test();
 }
 
-void
+void test(){
+  // Turn the first led red for 1 second
+  //leds[0] = CRGB::Red;
+  //FastLED.show();
+  //delay(1000);
+  // Set the first led back to black for 1 second
+  //leds[0] = CRGB::Black; FastLED.show();
+  for(int i = 0; i<NUM_LEDS; i++){
+    leds[i] = CRGB::Red;
+    FastLED.show();
+    delay(100);
+    leds[i] = CRGB::Black;
+    FastLED.show();
+  }
+  //delay(1000);
+}
